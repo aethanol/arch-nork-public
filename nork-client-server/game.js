e0697c37edc1f0a6a092fc447ebeab9155fa67b0
Ethan Anderson <aethan@uw.edu>
2016-03-01 18:02:07 -0800

    fix game logic

--- nork-client-server/game.js
+++ nork-client-server/game.js
@@ -13,33 +13,83 @@ class Game {
     
     play() {
         
+        return 'Welcome to nork! \n' + world.rooms[0].description + '\n';
     }
     
-    input() {
+    // method to parse user input and invoke proper function
+    input(input) {
+        // parse text to lowercase and split text by spaces to string array
+        var text = input.toLowerCase().split(' ');
         
+        switch(text[0]){
+        case 'go' :
+            return this.go(text[1]);
+        case 'use' :
+            return this.use(text[1]);
+        case 'take' :
+            return this.take(text[1]);
+        case 'drop' :
+            return this.drop(text[1]);
+        case 'inventory' :
+            return this.inventory();
+        case 'brief' :
+            return this.brief();
+        default :   
+            return 'That command was not understood'; 
+        }
     }
     
     go(direction) {
-        if (this._currRoom.exits[direction] != undefined) {
-            this._currRoom = world.rooms[this._currRoom.exits[direction]];
-            return true;
+        if (this._currRoom.exits[direction] !== undefined) {
+            this._currRoom = this.findRoom(this._currRoom.exits[direction].id);
+            return this._currRoom.description;
          // if the direction is not valid print error
         } else {
             return "you can't go that way";
+        } 
+        
+    }
+    
+    // helper function to loop through rooms and find the right room
+    findRoom(id) {
+        for(var i = 0; i < world.rooms.length; i++){
+           
+            if(world.rooms[i].id === id){
+                return world.rooms[i];
+            }
         }
     }
     
     take(item) {
-        if(this._currRoom[item]){
+        var roomIndex = this._currRoom.items.indexOf(item);
+        var invIndex = this._inventory.indexOf(item);
+        // if index == -1 then that room does not contain item
+        // also check that you don't already have item
+        if(roomIndex !== -1 && invIndex === -1){
             this._inventory.push(item);
             return "It is a " + item + "! You add it to your inventory";
         } else {
             return "you can't take that!";
         }
+        // if(this._currRoom['items'].includes(item)){
+        //     //this._inventory.push(item);
+        //     
+        // } else {
+        //     
+        // }
+        
     }
     
     use(item) {
         
+    }
+    
+    inventory(){
+        var inv = '';
+        for(var i = 0; i < this._inventory.length; i++){
+            inv +=(' ' + this._inventory[i]);
+        }
+        return 'You have:' + inv;
     }
        
 }
---
+++ nork-client-server/server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+const game = require('./game.js'),// import game client
+    net = require('net'), //import socket module
+    server = net.createServer(); //create socket server
+
+//notify (via observer!) when a a connection occurs
+server.on('connection', function(socket) {
+
+    //we've established a socket to use
+
+    //send a message to the socket
+    socket.write('connected on: ' + socket.remoteAddress +':'+ socket.remotePort +'\n');
+    socket.write('starting game \n');
+    // invoke game kick off method
+    socket.write(game.play());
+    
+    // handle data events by interacting with the game logic
+    socket.on('data', function(data) {
+    
+        var input = data.toString().toLowerCase();
+        
+        //call input() on the game logic
+        // and output to the socket what the game returns    
+        socket.write(game.input(input));
+
+    });
+    
+    // handle if the client closes the connection
+    socket.on('close', function(){
+        console.log('client disconnected');
+        console.log('closing server');
+        process.exit(0);
+    });
+});
+                            
+//when we start "listening" for connections
+server.on('listening', function() {
+    //get address info
+    var addr = server.address();
+
+    //print the info
+    console.log('server listening on port %d', addr.port);  
+});
+
+server.listen(3000); //listen on port 3000
+
+
+
+
+
